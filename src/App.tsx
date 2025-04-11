8622983fdcef29b7272784c2da927744fd91541d
kazuya <Walk.aaaiaua@gmail.com>
2025-04-12 07:11:38 +0900

    ローディングスピナーの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SearchBar from "./SearchBar";
@@ -8,6 +8,18 @@ import Header from "./Header";
 
 const App: React.FC = () => {
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(()=>{
+    const fetchData =() =>{
+      setTimeout(() =>{
+console.log("4秒経過");
+      setLoading(false);
+      },4000);
+
+    };
+    fetchData();
+  },[]);
 
   const handleSearch = async (query: string) => {
     const results = await fetchRecipes(query);
@@ -17,8 +29,15 @@ const App: React.FC = () => {
   return (
     <div className="container mx-auto">
       <Header />
+      {loading?(
+        <p className="loader">ロード中。。。</p>
+      ): (
+        <>
       <SearchBar onSearch={handleSearch} />
       <Recipi recipes={recipes} />
+        </>
+      )}
+
     </div>
   );
 };
